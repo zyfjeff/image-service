e5c6c3032096ba4d759892f04b9a68bedbf3f42d
Peng Tao <bergwolf@hyper.sh>
2020-02-21 17:50:23 +0800

    rafs: drop dag module
    We do not need it to implement a fs tree.
    
    Signed-off-by: Peng Tao <bergwolf@hyper.sh>

--- rafs/src/lib.rs
+++ rafs/src/lib.rs
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#[allow(dead_code)]
-mod dag;
 #[allow(dead_code, unused_variables)]
 mod fs;
 #[allow(dead_code)]
