af98506da40d3f757d41564bf31db173fe39b85a
yansong.ys <yansong.ys@antfin.com>
2020-04-10 02:57:05 +0000

    rafs: cargo fmt

--- rafs/src/storage/backend/mod.rs
+++ rafs/src/storage/backend/mod.rs
@@ -16,16 +16,16 @@ use std::io::Result;
 
 // Rafs blob backend API
 pub trait BlobBackend {
-  // Initialize the blob backend
-  // Each backend should define its own config type
-  fn init(&mut self, config: HashMap<&str, &str>) -> Result<()>;
+    // Initialize the blob backend
+    // Each backend should define its own config type
+    fn init(&mut self, config: HashMap<&str, &str>) -> Result<()>;
 
-  // Read a range of data from blob into the provided slice
-  fn read(&self, blobid: &str, buf: &mut Vec<u8>, offset: u64, count: usize) -> Result<usize>;
+    // Read a range of data from blob into the provided slice
+    fn read(&self, blobid: &str, buf: &mut Vec<u8>, offset: u64, count: usize) -> Result<usize>;
 
-  // Write a range of data to blob from the provided slice
-  fn write(&self, blobid: &str, buf: &Vec<u8>, offset: u64) -> Result<usize>;
+    // Write a range of data to blob from the provided slice
+    fn write(&self, blobid: &str, buf: &Vec<u8>, offset: u64) -> Result<usize>;
 
-  // Close a backend
-  fn close(&mut self);
+    // Close a backend
+    fn close(&mut self);
 }
--- rafs/src/storage/backend/request.rs
+++ rafs/src/storage/backend/request.rs
@@ -12,98 +12,98 @@ use std::io::{Error, ErrorKind};
 pub struct ReqErr {}
 
 impl ReqErr {
-  pub fn inv_input<E: std::fmt::Debug>(err: E) -> Error {
-    Error::new(ErrorKind::InvalidInput, format!("{:?}", err))
-  }
-  pub fn inv_data<E: std::fmt::Debug>(err: E) -> Error {
-    Error::new(ErrorKind::InvalidData, format!("{:?}", err))
-  }
-  pub fn other<E: std::fmt::Debug>(err: E) -> Error {
-    Error::new(ErrorKind::Other, format!("{:?}", err))
-  }
-  pub fn broken_pipe<E: std::fmt::Debug>(err: E) -> Error {
-    Error::new(ErrorKind::BrokenPipe, format!("{:?}", err))
-  }
+    pub fn inv_input<E: std::fmt::Debug>(err: E) -> Error {
+        Error::new(ErrorKind::InvalidInput, format!("{:?}", err))
+    }
+    pub fn inv_data<E: std::fmt::Debug>(err: E) -> Error {
+        Error::new(ErrorKind::InvalidData, format!("{:?}", err))
+    }
+    pub fn other<E: std::fmt::Debug>(err: E) -> Error {
+        Error::new(ErrorKind::Other, format!("{:?}", err))
+    }
+    pub fn broken_pipe<E: std::fmt::Debug>(err: E) -> Error {
+        Error::new(ErrorKind::BrokenPipe, format!("{:?}", err))
+    }
 }
 
 pub struct Progress {
-  inner: File,
-  current: u64,
-  total: u64,
-  callback: fn((u64, u64)),
+    inner: File,
+    current: u64,
+    total: u64,
+    callback: fn((u64, u64)),
 }
 
 impl Progress {
-  pub fn new(file: File, total: u64, callback: fn((u64, u64))) -> Progress {
-    Progress {
-      inner: file,
-      current: 0,
-      total,
-      callback,
+    pub fn new(file: File, total: u64, callback: fn((u64, u64))) -> Progress {
+        Progress {
+            inner: file,
+            current: 0,
+            total,
+            callback,
+        }
     }
-  }
 }
 
 impl Read for Progress {
-  fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
-    self.inner.read(buf).map(|count| {
-      self.current += count as u64;
-      (self.callback)((self.current, self.total));
-      count
-    })
-  }
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        self.inner.read(buf).map(|count| {
+            self.current += count as u64;
+            (self.callback)((self.current, self.total));
+            count
+        })
+    }
 }
 
 pub enum FileBody {
-  File(Progress, u64),
-  Buf(Vec<u8>),
+    File(Progress, u64),
+    Buf(Vec<u8>),
 }
 
 #[derive(Debug)]
 pub struct Request {
-  client: Client,
+    client: Client,
 }
 
 impl Request {
-  pub fn new() -> Request {
-    let client = Client::builder().timeout(None).build().unwrap();
-    Request { client }
-  }
-
-  pub fn call(
-    &self,
-    method: &str,
-    url: &str,
-    data: FileBody,
-    headers: HeaderMap,
-  ) -> Result<Response> {
-    debug!("request {:?} method {:?} url {:?}", headers, method, url);
-
-    let method = Method::from_bytes(method.as_bytes()).map_err(ReqErr::inv_input)?;
-
-    let rb = self.client.request(method, url).headers(headers);
-
-    let ret;
-    match data {
-      FileBody::File(body, total) => {
-        let body = Body::sized(body, total);
-        ret = rb.body(body).send();
-      }
-      FileBody::Buf(buf) => {
-        ret = rb.body(buf).send();
-      }
+    pub fn new() -> Request {
+        let client = Client::builder().timeout(None).build().unwrap();
+        Request { client }
     }
 
-    match ret {
-      Ok(resp) => {
-        let status = resp.status();
-        if status >= StatusCode::OK && status < StatusCode::MULTIPLE_CHOICES {
-          return Ok(resp);
+    pub fn call(
+        &self,
+        method: &str,
+        url: &str,
+        data: FileBody,
+        headers: HeaderMap,
+    ) -> Result<Response> {
+        debug!("request {:?} method {:?} url {:?}", headers, method, url);
+
+        let method = Method::from_bytes(method.as_bytes()).map_err(ReqErr::inv_input)?;
+
+        let rb = self.client.request(method, url).headers(headers);
+
+        let ret;
+        match data {
+            FileBody::File(body, total) => {
+                let body = Body::sized(body, total);
+                ret = rb.body(body).send();
+            }
+            FileBody::Buf(buf) => {
+                ret = rb.body(buf).send();
+            }
         }
-        let message = resp.text().map_err(ReqErr::broken_pipe)?;
-        Err(Error::new(ErrorKind::Other, message))
-      }
-      Err(err) => Err(Error::new(ErrorKind::Other, format!("{}", err))),
+
+        match ret {
+            Ok(resp) => {
+                let status = resp.status();
+                if status >= StatusCode::OK && status < StatusCode::MULTIPLE_CHOICES {
+                    return Ok(resp);
+                }
+                let message = resp.text().map_err(ReqErr::broken_pipe)?;
+                Err(Error::new(ErrorKind::Other, message))
+            }
+            Err(err) => Err(Error::new(ErrorKind::Other, format!("{}", err))),
+        }
     }
-  }
 }
