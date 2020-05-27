d861bc6eec45768d6c3a1ee28599142a9f4986b2
Peng Tao <bergwolf@hyper.sh>
2020-05-27 23:03:56 +0800

    license: change from BSD 3-clause to Apache 2.0
    To make it easier to go public.
    
    Signed-off-by: Peng Tao <bergwolf@hyper.sh>

--- image_builder/src/builder.rs
+++ image_builder/src/builder.rs
@@ -1,5 +1,5 @@
 // Copyright 2020 Ant Financial. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
+// Use of this source code is governed by a Apache 2.0 license that can be
 // found in the LICENSE file.
 
 use crypto::digest::Digest;
