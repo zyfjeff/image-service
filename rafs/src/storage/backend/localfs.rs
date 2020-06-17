b66c14ac601724dc444815bf022cc5ca6f27242c
Peng Tao <bergwolf@hyper.sh>
2020-06-17 19:25:42 +0800

    localfs: use mmap to read access log file
    So that we can share page cache across multiple instances.
    
    Signed-off-by: Peng Tao <bergwolf@hyper.sh>

--- rafs/src/storage/backend/localfs.rs
+++ rafs/src/storage/backend/localfs.rs
@@ -4,7 +4,8 @@
 
 use std::collections::HashMap;
 use std::fs::{self, remove_file, File, OpenOptions};
-use std::io::{self, BufRead, BufReader, Error, Result, Write};
+use std::io::{self, Error, Result};
+use std::mem::{size_of, ManuallyDrop};
 use std::os::unix::io::{AsRawFd, RawFd};
 use std::path::Path;
 use std::sync::{Arc, Mutex, RwLock};
@@ -16,14 +17,16 @@ use vm_memory::VolatileSlice;
 use crate::storage::backend::ReqErr;
 use crate::storage::backend::{BlobBackend, BlobBackendUploader};
 use crate::storage::utils::readv;
+use crate::{ebadf, einval};
 use nydus_utils::{round_down_4k, round_up_4k};
 
-const BLOB_ACCESSED_SPLITTER: &str = "\t";
 const BLOB_ACCESSED_SUFFIX: &str = ".access";
 
-// Each access record takes 18 bytes: u64 + usize + "\t" + "\n"
-// So we allow 1820 at most to avoid hurting backend upon flush
-const MAX_ACCESS_RECORD: usize = 32768 / 18;
+// Each access record takes 16 bytes: u64 + u32 + u32
+// So we allow 2048 entries at most to avoid hurting backend upon flush
+const ACCESS_RECORD_ENTRY_SIZE: usize = size_of::<u64>() + size_of::<u32>() + size_of::<u32>();
+const MAX_ACCESS_RECORD_FILE_SIZE: usize = 32768;
+const MAX_ACCESS_RECORD: usize = MAX_ACCESS_RECORD_FILE_SIZE / ACCESS_RECORD_ENTRY_SIZE;
 
 type FileTableEntry = (File, Option<Arc<LocalFsAccessLog>>);
 
@@ -64,100 +67,202 @@ pub fn new<S: std::hash::BuildHasher>(config: &HashMap<String, String, S>) -> Re
     }
 }
 
+type AccessLogEntry = (u64, u32, u32);
+
+// Access entries can be either mmapped or Vec-allocated.
+// Use mmap for read case and Vec-allocated for write case.
 struct LocalFsAccessLog {
-    path: String,                      // access log file path
-    fd: RawFd,                         // blob fd for readahead
-    file: Mutex<File>,                 // access log file
-    records: Mutex<Vec<(u64, usize)>>, // access records
+    log_path: String,                                  // log file path
+    log_fd: RawFd,                                     // log file fd
+    log_base: *const u8,                               // mmaped access log base
+    log_size: usize,                                   // log file size
+    blob_fd: RawFd,                                    // blob fd for readahead
+    blob_size: usize,                                  // blob file size
+    records: ManuallyDrop<Mutex<Vec<AccessLogEntry>>>, // access records
 }
 
+unsafe impl Send for LocalFsAccessLog {}
+
+unsafe impl Sync for LocalFsAccessLog {}
+
 impl LocalFsAccessLog {
-    fn new(file: File, path: String, fd: RawFd) -> LocalFsAccessLog {
+    fn new() -> LocalFsAccessLog {
         LocalFsAccessLog {
-            path,
-            fd,
-            file: Mutex::new(file),
-            records: Mutex::new(Vec::new()),
+            log_path: "".to_string(),
+            log_fd: -1,
+            log_base: std::ptr::null(),
+            log_size: 0,
+            blob_fd: -1,
+            blob_size: 0,
+            records: ManuallyDrop::new(Mutex::new(Vec::new())),
         }
+    }
+
+    fn init(
+        &mut self,
+        log_file: File,
+        log_path: String,
+        blob_fd: RawFd,
+        blob_size: usize,
+        load_entries: bool,
+    ) -> Result<()> {
+        if self.log_fd > 0
+            || !self.log_path.is_empty()
+            || self.blob_fd > 0
+            || self.records.lock().unwrap().len() > 0
+        {
+            return Err(einval());
+        }
+
+        self.log_fd = unsafe { libc::dup(log_file.as_raw_fd()) };
+        if self.log_fd < 0 {
+            return Err(Error::last_os_error());
+        }
+        self.blob_fd = unsafe { libc::dup(blob_fd) };
+        if self.blob_fd < 0 {
+            return Err(Error::last_os_error());
+        }
+        self.log_path = log_path;
+        self.blob_size = blob_size;
+
+        if !load_entries {
+            return Ok(());
+        }
+
+        // load exiting entries
+        let size = log_file.metadata()?.len() as usize;
+        if size == 0 || size % ACCESS_RECORD_ENTRY_SIZE != 0 {
+            warn!("ignoring unaligned log file");
+            return Ok(());
+        }
+        let count = size / ACCESS_RECORD_ENTRY_SIZE;
+        let base = unsafe {
+            libc::mmap(
+                std::ptr::null_mut(),
+                size as usize,
+                libc::PROT_READ,
+                libc::MAP_NORESERVE | libc::MAP_PRIVATE,
+                self.log_fd,
+                0,
+            )
+        } as *const AccessLogEntry;
+        if base as *mut core::ffi::c_void == libc::MAP_FAILED {
+            return Err(Error::last_os_error());
+        }
+        if base.is_null() {
+            return Err(ebadf());
+        }
+        // safe because we have validated size
+        self.records = unsafe {
+            ManuallyDrop::new(Mutex::new(Vec::from_raw_parts(
+                base as *mut AccessLogEntry,
+                count as usize,
+                count as usize,
+            )))
+        };
+        self.log_base = base as *const u8;
+        self.log_size = size;
+        Ok(())
     }
 
     fn do_readahead(&self) -> Result<()> {
         info!("starting localfs blob readahead");
-        let file = self.file.lock().unwrap();
-        for line in BufReader::new(&(*file)).lines() {
-            if let Ok(line) = line {
-                let v: Vec<&str> = line.split(BLOB_ACCESSED_SPLITTER).collect();
-                if v.len() != 2 {
-                    warn!("localfs blob access log invalid entry: {}", line);
-                    return Err(Error::from_raw_os_error(libc::EINVAL));
-                }
-                let offset: i64 = v[0].parse().map_err(|_| {
-                    warn!("localfs blob access log invalid entry: {}", line);
-                    Error::from_raw_os_error(libc::EINVAL)
-                })?;
-                let len: usize = v[1].parse().map_err(|_| {
-                    warn!("localfs blob access log invalid entry: {}", line);
-                    Error::from_raw_os_error(libc::EINVAL)
-                })?;
-                unsafe { libc::readahead(self.fd, offset, len) };
+        for &(offset, len, zero) in self.records.lock().unwrap().iter() {
+            let end: u64 = offset.checked_add(len as u64).ok_or_else(einval)?;
+            if offset > self.blob_size as u64 || end > self.blob_size as u64 || zero != 0 {
+                warn!(
+                    "invalid readahead entry ({}, {}), blob size {}",
+                    offset, len, self.blob_size
+                );
+                return Err(einval());
             }
+            unsafe { libc::readahead(self.blob_fd, offset as i64, len as usize) };
         }
         Ok(())
     }
 
     fn record(&self, offset: u64, len: u32) {
+        // Never modify mmaped records
+        if !self.log_base.is_null() {
+            return;
+        }
+
         let mut r = self.records.lock().unwrap();
         if r.len() < MAX_ACCESS_RECORD {
             r.push((
                 round_down_4k(offset),
                 // Safe to unwrap because len is u32
-                round_up_4k(len as u64).unwrap() as usize,
+                round_up_4k(len as u64).unwrap() as u32,
+                0,
             ));
         }
     }
 
     fn flush(&self) {
-        info!("flushing access log to {}", &self.path);
-        let mut record: Vec<String> = Vec::new();
+        info!("flushing access log to {}", &self.log_path);
         let mut r = self.records.lock().unwrap();
         if r.len() == 0 {
             info!(
                 "No read access is recorded. Drop access file {}",
-                &self.path
+                &self.log_path
             );
+            // set record length to max to no new record is saved
+            // safe becasue we have locked records
+            unsafe { r.set_len(MAX_ACCESS_RECORD) };
             drop(r);
-            if let Err(e) = remove_file(Path::new(&self.path)) {
-                warn!("failed to remove access file {}: {}", &self.path, e);
+            if let Err(e) = remove_file(Path::new(&self.log_path)) {
+                warn!("failed to remove access file {}: {}", &self.log_path, e);
             }
             return;
         }
         r.sort();
         r.dedup();
-        for (offset, len) in r.iter() {
-            record.push(
-                vec![
-                    offset.to_string(),
-                    BLOB_ACCESSED_SPLITTER.to_string(),
-                    len.to_string(),
-                ]
-                .join(""),
-            )
-        }
+        let record = r.clone();
         r.clear();
         // set record length to max to no new record is saved
         // safe becasue we have locked records
         unsafe { r.set_len(MAX_ACCESS_RECORD) };
         drop(r);
 
-        let _ = self
-            .file
-            .lock()
-            .unwrap()
-            .write_all((record.join("\n") + "\n").as_bytes())
-            .map_err(|e| {
-                warn!("fail to write access log: {}", e);
-                e
-            });
+        let record = unsafe {
+            std::slice::from_raw_parts(
+                record.as_ptr() as *const u8,
+                record.len() * std::mem::size_of::<AccessLogEntry>(),
+            )
+        };
+
+        let _ = nix::unistd::write(self.log_fd, record).map_err(|e| {
+            warn!("fail to write access log: {}", e);
+            e
+        });
+    }
+}
+
+impl Drop for LocalFsAccessLog {
+    fn drop(&mut self) {
+        if !self.log_base.is_null() {
+            unsafe {
+                libc::munmap(
+                    self.log_base as *mut u8 as *mut libc::c_void,
+                    self.log_size as usize,
+                )
+            };
+            self.log_base = std::ptr::null();
+            self.log_size = 0;
+        } else {
+            // Drop records if it is not mmapped
+            unsafe {
+                ManuallyDrop::drop(&mut self.records);
+            }
+        }
+        if self.blob_fd > 0 {
+            let _ = nix::unistd::close(self.blob_fd);
+            self.blob_fd = -1;
+        }
+        if self.log_fd > 0 {
+            let _ = nix::unistd::close(self.log_fd);
+            self.log_fd = -1;
+        }
     }
 }
 
@@ -186,6 +291,7 @@ impl LocalFs {
                 error!("failed to open blob {}: {}", blob_id, e);
                 e
             })?;
+        let size = file.metadata()?.len() as usize;
         let fd = file.as_raw_fd();
 
         // Don't expect poisoned lock here.
@@ -212,16 +318,18 @@ impl LocalFs {
             .read(true)
             .open(Path::new(&access_file_path))
         {
+            table_guard.insert(blob_id.to_string(), (file, None));
+            drop(table_guard);
             // Found access log, kick off readahead
-            if file.metadata().unwrap().len() > 0 {
-                let access_log = LocalFsAccessLog::new(access_file, access_file_path, fd);
+            if size > 0 {
+                let mut access_log = LocalFsAccessLog::new();
+                access_log.init(access_file, access_file_path, fd, size, true)?;
                 let _ = thread::Builder::new()
                     .name("nydus-localfs-readahead".to_string())
                     .spawn(move || {
                         let _ = access_log.do_readahead();
                     });
             }
-            table_guard.insert(blob_id.to_string(), (file, None));
             return Ok(fd);
         }
 
@@ -232,12 +340,15 @@ impl LocalFs {
             .create_new(true)
             .open(Path::new(&access_file_path))
         {
-            let access_log = Arc::new(LocalFsAccessLog::new(access_file, access_file_path, fd));
+            let mut access_log = LocalFsAccessLog::new();
+            access_log.init(access_file, access_file_path, fd, size, false)?;
             // Log the first access
             if len != 0 {
                 access_log.record(offset, len as u32);
             }
+            let access_log = Arc::new(access_log);
             table_guard.insert(blob_id.to_string(), (file, Some(access_log.clone())));
+            drop(table_guard);
             // Split a thread to flush access record
             let _ = thread::Builder::new()
                 .name("nydus-localfs-access-recorder".to_string())
