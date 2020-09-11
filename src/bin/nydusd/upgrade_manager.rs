1a58850c728c9537d8461e190625eadb32637cdd
Changwei Ge <chge@linux.alibaba.com>
2020-09-11 18:19:14 +0800

    nydusd: initial implementation for upgrade manager
    Signed-off-by: Changwei Ge <chge@linux.alibaba.com>

---
+++ src/bin/nydusd/upgrade_manager.rs
@@ -0,0 +1,49 @@
+// Copyright 2020 Ant Financial. All rights reserved.
+// Copyright (C) 2020 Alibaba Cloud. All rights reserved.
+//
+// SPDX-License-Identifier: (Apache-2.0 AND BSD-3-Clause)
+
+use std::collections::HashMap;
+use std::io::Result;
+use std::sync::{Arc, Mutex};
+
+lazy_static! {
+    pub static ref UPGRADE_MRG: Mutex<UpgradeManager> = Mutex::new(UpgradeManager::new());
+}
+#[derive(Default)]
+pub struct UpgradeManager {
+    resources: HashMap<ResourceType, Arc<dyn Resource + Sync + Send>>,
+}
+
+#[derive(Hash, PartialEq, Eq)]
+#[allow(dead_code)]
+pub enum ResourceType {
+    Fd,
+    Binary,
+}
+
+#[allow(dead_code)]
+impl UpgradeManager {
+    fn new() -> Self {
+        UpgradeManager {
+            ..Default::default()
+        }
+    }
+
+    pub fn add_resource<R: Resource + Sync + Send + 'static>(&mut self, r: R, t: ResourceType) {
+        if self.resources.insert(t, Arc::new(r)).is_some() {
+            debug!("Already exists");
+        }
+    }
+
+    pub fn get_resource(&self, t: ResourceType) -> Option<&(dyn Resource + Send + Sync)> {
+        self.resources.get(&t).map(|r| r.as_ref())
+    }
+
+    pub fn del_resource(&mut self, _res: String) {}
+}
+
+pub trait Resource {
+    fn load(&self) -> Result<()>;
+    fn store(&self) -> Result<()>;
+}
