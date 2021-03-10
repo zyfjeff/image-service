92a4539d93e20472a2ab1cd6d827da9a336140e4
Yan Song <imeoer@linux.alibaba.com>
2021-03-10 10:20:00 +0000

    Smoke: remove unnecessary result check
    Replace `Result<T>` with `unwrap`, result check is unnecessary
    in rust tests.
    
    Signed-off-by: Yan Song <imeoer@linux.alibaba.com>

--- tests/smoke.rs
+++ tests/smoke.rs
@@ -8,12 +8,11 @@ mod builder;
 mod nydusd;
 mod snapshotter;
 
-use std::io::Result;
 use std::path::PathBuf;
 
 use vmm_sys_util::tempdir::TempDir;
 
-use nydus_utils::{eother, exec, setup_logging};
+use nydus_utils::{exec, setup_logging};
 use snapshotter::Snapshotter;
 
 const COMPAT_BOOTSTRAPS: [&str; 2] = [
@@ -21,22 +20,20 @@ const COMPAT_BOOTSTRAPS: [&str; 2] = [
     "sha256-nocompress-repeatable",
 ];
 
-fn check_compact<'a>(work_dir: &'a PathBuf, bootstrap_name: &str, rafs_mode: &str) -> Result<()> {
+fn check_compact<'a>(work_dir: &'a PathBuf, bootstrap_name: &str, rafs_mode: &str) {
     let nydusd = nydusd::new(
         work_dir,
         false,
         false,
-        rafs_mode.parse()?,
+        rafs_mode.parse().unwrap(),
         "api.sock".into(),
         true,
-    )?;
+    );
 
-    nydusd.start(Some(bootstrap_name), "mnt")?;
+    nydusd.start(Some(bootstrap_name), "mnt");
     let result_path = format!("repeatable/{}.result", bootstrap_name);
-    nydusd.check(result_path.as_str(), "mnt")?;
+    nydusd.check(result_path.as_str(), "mnt");
     nydusd.umount("mnt");
-
-    Ok(())
 }
 
 fn test(
@@ -45,7 +42,7 @@ fn test(
     cache_compressed: bool,
     rafs_mode: &str,
     whiteout_spec: &str,
-) -> Result<()> {
+) {
     // std::thread::sleep(std::time::Duration::from_secs(1000));
 
     info!(
@@ -65,7 +62,7 @@ fn test(
         } else {
             format!("{}/", tmp_dir_prefix)
         };
-        TempDir::new_with_prefix(path).map_err(|e| eother!(e))?
+        TempDir::new_with_prefix(path).unwrap()
     };
     let work_dir = tmp_dir.as_path().to_path_buf();
     let lower_texture = "directory/lower.result".to_string();
@@ -75,40 +72,40 @@ fn test(
 
     {
         // Create & build lower rootfs
-        builder.make_lower()?;
-        builder.build_lower(compressor)?;
+        builder.make_lower();
+        builder.build_lower(compressor);
 
         // Mount lower rootfs and check
         let nydusd = nydusd::new(
             &work_dir,
             enable_cache,
             cache_compressed,
-            rafs_mode.parse()?,
+            rafs_mode.parse().unwrap(),
             "api.sock".into(),
             true,
-        )?;
-        nydusd.start(Some("bootstrap-lower"), "mnt")?;
-        nydusd.check(&lower_texture, "mnt")?;
+        );
+        nydusd.start(Some("bootstrap-lower"), "mnt");
+        nydusd.check(&lower_texture, "mnt");
         nydusd.umount("mnt");
     }
 
     // Mount upper rootfs and check
     {
         // Create & build upper rootfs based lower
-        builder.make_upper()?;
-        builder.build_upper(compressor)?;
+        builder.make_upper();
+        builder.build_upper(compressor);
 
         // Mount overlay rootfs and check
         let nydusd = nydusd::new(
             &work_dir,
             enable_cache,
             cache_compressed,
-            rafs_mode.parse()?,
+            rafs_mode.parse().unwrap(),
             "api.sock".into(),
             true,
-        )?;
-        nydusd.start(Some("bootstrap-overlay"), "mnt")?;
-        nydusd.check(&overlay_texture, "mnt")?;
+        );
+        nydusd.start(Some("bootstrap-overlay"), "mnt");
+        nydusd.check(&overlay_texture, "mnt");
         nydusd.umount("mnt");
     }
 
@@ -118,16 +115,14 @@ fn test(
             &work_dir,
             enable_cache,
             cache_compressed,
-            rafs_mode.parse()?,
+            rafs_mode.parse().unwrap(),
             "api.sock".into(),
             true,
-        )?;
-        nydusd.start(Some("bootstrap-overlay"), "mnt")?;
-        nydusd.check(&overlay_texture, "mnt")?;
+        );
+        nydusd.start(Some("bootstrap-overlay"), "mnt");
+        nydusd.check(&overlay_texture, "mnt");
         nydusd.umount("mnt");
     }
-
-    Ok(())
 }
 
 #[test]
@@ -136,135 +131,130 @@ fn integration_test_init() {
 }
 
 #[test]
-fn integration_test_directory_1() -> Result<()> {
+fn integration_test_directory_1() {
     test("lz4_block", true, false, "direct", "oci")
 }
 
 #[test]
-fn integration_test_directory_2() -> Result<()> {
+fn integration_test_directory_2() {
     test("lz4_block", false, false, "direct", "oci")
 }
 
 #[test]
-fn integration_test_directory_3() -> Result<()> {
+fn integration_test_directory_3() {
     test("gzip", false, false, "direct", "oci")
 }
 
 #[test]
-fn integration_test_directory_4() -> Result<()> {
+fn integration_test_directory_4() {
     test("none", true, false, "direct", "oci")
 }
 
 #[test]
-fn integration_test_directory_5() -> Result<()> {
+fn integration_test_directory_5() {
     test("gzip", true, true, "cached", "oci")
 }
 
 #[test]
-fn integration_test_directory_6() -> Result<()> {
+fn integration_test_directory_6() {
     test("none", false, true, "cached", "oci")
 }
 
 #[test]
-fn integration_test_directory_7() -> Result<()> {
+fn integration_test_directory_7() {
     test("lz4_block", false, true, "cached", "oci")
 }
 
 #[test]
-fn integration_test_directory_8() -> Result<()> {
+fn integration_test_directory_8() {
     test("lz4_block", true, true, "cached", "oci")
 }
 
 #[test]
-fn integration_test_directory_9() -> Result<()> {
+fn integration_test_directory_9() {
     test("lz4_block", true, false, "direct", "overlayfs")
 }
 
 #[test]
-fn integration_test_compact() -> Result<()> {
+fn integration_test_compact() {
     info!("\n\n==================== testing run: compact test");
 
-    let tmp_dir = TempDir::new().map_err(|e| eother!(e))?;
+    let tmp_dir = TempDir::new().unwrap();
     let work_dir = tmp_dir.as_path().to_path_buf();
     let _ = exec(
         format!("cp -a tests/texture/repeatable/* {:?}", work_dir).as_str(),
         false,
-    )?;
+    );
 
     for mode in vec!["direct", "cached"].iter() {
         for bs in COMPAT_BOOTSTRAPS.iter() {
-            check_compact(&work_dir, bs, mode)?;
+            check_compact(&work_dir, bs, mode);
         }
     }
-
-    Ok(())
 }
 
 #[test]
-fn integration_test_special_files() -> Result<()> {
+fn integration_test_special_files() {
     info!("\n\n==================== testing run: special file test");
-    let tmp_dir = TempDir::new().map_err(|e| eother!(e))?;
+    let tmp_dir = TempDir::new().unwrap();
     let work_dir = tmp_dir.as_path().to_path_buf();
 
     let mut builder = builder::new(&work_dir, "oci");
 
-    builder.build_special_files()?;
+    builder.build_special_files();
 
     for mode in &["direct", "cached"] {
         let nydusd = nydusd::new(
             &work_dir,
             true,
             true,
-            mode.parse()?,
+            mode.parse().unwrap(),
             "api.sock".into(),
             false,
-        )?;
-        nydusd.start(Some("bootstrap-specialfiles"), "mnt")?;
-        nydusd.check("specialfiles/result", "mnt")?;
+        );
+        nydusd.start(Some("bootstrap-specialfiles"), "mnt");
+        nydusd.check("specialfiles/result", "mnt");
         nydusd.umount("mnt");
     }
-
-    Ok(())
 }
 
 #[test]
-fn integration_test_stargz() -> Result<()> {
+fn integration_test_stargz() {
     info!("\n\n==================== testing run: stargz test");
 
-    let tmp_dir = TempDir::new().map_err(|e| eother!(e))?;
+    let tmp_dir = TempDir::new().unwrap();
     let work_dir = tmp_dir.as_path().to_path_buf();
 
     let _ = exec(
         format!("cp -a tests/texture/stargz/* {:?}", work_dir).as_str(),
         false,
-    )?;
+    )
+    .unwrap();
 
     let mut builder = builder::new(&work_dir, "oci");
 
-    builder.build_stargz_lower()?;
-    builder.build_stargz_upper()?;
+    builder.build_stargz_lower();
+    builder.build_stargz_upper();
 
     let nydusd = nydusd::new(
         &work_dir,
         true,
         true,
-        "direct".parse()?,
+        "direct".parse().unwrap(),
         "api.sock".into(),
         false,
-    )?;
+    );
 
-    nydusd.start(Some("bootstrap-overlay"), "mnt")?;
-    nydusd.check("directory/overlay.result", "mnt")?;
+    nydusd.start(Some("bootstrap-overlay"), "mnt");
+    nydusd.check("directory/overlay.result", "mnt");
     nydusd.umount("mnt");
-
-    Ok(())
 }
 
 #[test]
-fn integration_test_hot_upgrade_with_fusedev() -> Result<()> {
+fn integration_test_hot_upgrade_with_fusedev() {
     info!("\n\n==================== testing run: hot upgrade with fusedev");
 
-    let tmp_dir = TempDir::new().map_err(|e| eother!(e))?;
+    let tmp_dir = TempDir::new().unwrap();
     let work_dir = tmp_dir.as_path().to_path_buf();
     let _ = exec(
         format!("cp -a tests/texture/repeatable/* {:?}", work_dir).as_str(),
@@ -276,24 +266,24 @@ fn integration_test_hot_upgrade_with_fusedev() -> Result<()> {
         &work_dir,
         false,
         false,
-        "direct".parse()?,
+        "direct".parse().unwrap(),
         "api.sock".into(),
         true,
-    )?;
+    );
 
     let new_nydusd = nydusd::new(
         &work_dir,
         false,
         false,
-        "direct".parse()?,
+        "direct".parse().unwrap(),
         "new-api.sock".into(),
         true,
-    )?;
+    );
 
     let snapshotter = Snapshotter::new(work_dir);
 
     // Start old nydusd to mount
-    old_nydusd.start(Some(COMPAT_BOOTSTRAPS[0]), "mnt")?;
+    old_nydusd.start(Some(COMPAT_BOOTSTRAPS[0]), "mnt");
 
     // Old nydusd's state should be RUNNING
     assert_eq!(snapshotter.get_status(&old_nydusd.api_sock), "RUNNING");
@@ -302,7 +292,7 @@ fn integration_test_hot_upgrade_with_fusedev() -> Result<()> {
     snapshotter.request_sendfd(&old_nydusd.api_sock);
 
     // Start new nydusd but don't mount
-    new_nydusd.start_with_upgrade(Some(COMPAT_BOOTSTRAPS[0]), "mnt")?;
+    new_nydusd.start_with_upgrade(Some(COMPAT_BOOTSTRAPS[0]), "mnt");
 
     // New nydusd's state should be INIT
     assert_eq!(snapshotter.get_status(&new_nydusd.api_sock), "INIT");
@@ -321,19 +311,17 @@ fn integration_test_hot_upgrade_with_fusedev() -> Result<()> {
 
     // Check files in mount point
     let result_path = format!("repeatable/{}.result", COMPAT_BOOTSTRAPS[0]);
-    new_nydusd.check(result_path.as_str(), "mnt")?;
+    new_nydusd.check(result_path.as_str(), "mnt");
 
     // Stop new nydusd
     new_nydusd.umount("mnt");
-
-    Ok(())
 }
 
 #[test]
-fn integration_test_hot_upgrade_with_rafs_mount() -> Result<()> {
+fn integration_test_hot_upgrade_with_rafs_mount() {
     info!("\n\n==================== testing run: hot upgrade with rafs mount");
 
-    let tmp_dir = TempDir::new().map_err(|e| eother!(e))?;
+    let tmp_dir = TempDir::new().unwrap();
     let work_dir = tmp_dir.as_path().to_path_buf();
     let _ = exec(
         format!("cp -a tests/texture/repeatable/* {:?}", work_dir).as_str(),
@@ -345,24 +333,24 @@ fn integration_test_hot_upgrade_with_rafs_mount() -> Result<()> {
         &work_dir,
         false,
         false,
-        "direct".parse()?,
+        "direct".parse().unwrap(),
         "api.sock".into(),
         true,
-    )?;
+    );
 
     let new_nydusd = nydusd::new(
         &work_dir,
         false,
         false,
-        "direct".parse()?,
+        "direct".parse().unwrap(),
         "new-api.sock".into(),
         true,
-    )?;
+    );
 
     let snapshotter = Snapshotter::new(work_dir);
 
     // Start old nydusd to mount
-    old_nydusd.start(None, "mnt")?;
+    old_nydusd.start(None, "mnt");
 
     // Add RAFS mountpoint
     snapshotter.mount(&old_nydusd.api_sock, "blobs", "/sub1", COMPAT_BOOTSTRAPS[0]);
@@ -377,7 +365,7 @@ fn integration_test_hot_upgrade_with_rafs_mount() -> Result<()> {
     snapshotter.request_sendfd(&old_nydusd.api_sock);
 
     // Start new nydusd but don't mount
-    new_nydusd.start_with_upgrade(None, "mnt")?;
+    new_nydusd.start_with_upgrade(None, "mnt");
 
     // New nydusd's state should be INIT
     assert_eq!(snapshotter.get_status(&new_nydusd.api_sock), "INIT");
@@ -396,15 +384,13 @@ fn integration_test_hot_upgrade_with_rafs_mount() -> Result<()> {
 
     // Check files in mount point
     let result_path = format!("repeatable/{}.result", COMPAT_BOOTSTRAPS[0]);
-    new_nydusd.check(result_path.as_str(), "mnt/sub1")?;
+    new_nydusd.check(result_path.as_str(), "mnt/sub1");
 
     let result_path = format!("repeatable/{}.result", COMPAT_BOOTSTRAPS[1]);
-    new_nydusd.check(result_path.as_str(), "mnt/sub2")?;
+    new_nydusd.check(result_path.as_str(), "mnt/sub2");
 
     snapshotter.umount(&new_nydusd.api_sock, "/sub2");
 
     // Stop new nydusd
     new_nydusd.umount("mnt");
-
-    Ok(())
 }
