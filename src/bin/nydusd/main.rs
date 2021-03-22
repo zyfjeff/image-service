7f5d312c69092ce1b82d0f6d953381a98ee85f2c
Changwei Ge <chge@linux.alibaba.com>
2021-03-22 17:53:42 +0800

    daemon: print program version aka crate version
    Print this version into log.
    
    Signed-off-by: Changwei Ge <chge@linux.alibaba.com>

--- src/bin/nydusd/main.rs
+++ src/bin/nydusd/main.rs
@@ -270,7 +270,7 @@ fn main() -> Result<()> {
 
     setup_logging(logging_file, level)?;
 
-    dump_program_info();
+    dump_program_info(crate_version!());
 
     // Retrieve arguments
     // shared-dir means fs passthrough
