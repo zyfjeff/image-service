zyfjeff/image-service

commit 1b6642d1473519de33cc518bbb84af76d078e24f

Builder: refactor with build context
Introduce BlobContext and BootstrapContext, BlobContext is used to hold
the blob information of a layer during build, BootstrapContext is used
to hold inmemory data of bootstrap during build. BlobManager holds all
BlobContext for all layers.

When building multi-layers image, binding a BlobContext for each layer
is very useful for blob management and simplifies blob-related operations
for next diff build implementation.

Signed-off-by: Yan Song <imeoer@linux.alibaba.com>

src/bin/nydus-image/builder/directory.rs
@@ -8,9 +8,9 @@ use std::fs;
 use std::fs::DirEntry;
 
 use crate::builder::Builder;
-use crate::core::blob::{Blob, BlobStorage};
+use crate::core::blob::Blob;
 use crate::core::bootstrap::Bootstrap;
-use crate::core::context::BuildContext;
+use crate::core::context::{BlobContext, BlobManager, BootstrapContext, BuildContext};
 use crate::core::node::*;
 use crate::core::tree::Tree;
 
@@ -22,14 +22,19 @@ impl FilesystemTreeBuilder {
     }
 
     /// Walk directory to build node tree by DFS
-    fn load_children(&self, mut ctx: &mut BuildContext, parent: &mut Node) -> Result<Vec<Tree>> {
+    fn load_children(
+        &self,
+        ctx: &mut BuildContext,
+        bootstrap_ctx: &mut BootstrapContext,
+        parent: &mut Node,
+    ) -> Result<Vec<Tree>> {
         let mut result = Vec::new();
 
         if !parent.is_dir() {
             return Ok(result);
         }
 
-        let layered = ctx.f_parent_bootstrap.is_some();
+        let layered = bootstrap_ctx.f_parent_bootstrap.is_some();
         let children = fs::read_dir(&parent.path)
             .with_context(|| format!("failed to read dir {:?}", parent.path))?;
         let children = children.collect::<Result<Vec<DirEntry>, std::io::Error>>()?;
@@ -57,7 +62,7 @@ impl FilesystemTreeBuilder {
             }
 
             let mut child = Tree::new(child);
-            child.children = self.load_children(&mut ctx, &mut child.node)?;
+            child.children = self.load_children(ctx, bootstrap_ctx, &mut child.node)?;
             result.push(child);
         }
 
@@ -65,17 +70,19 @@ impl FilesystemTreeBuilder {
     }
 }
 
-pub struct DirectoryBuilder {
-    blob_stor: BlobStorage,
-}
+pub struct DirectoryBuilder {}
 
 impl DirectoryBuilder {
-    pub fn new(blob_stor: BlobStorage) -> Self {
-        Self { blob_stor }
+    pub fn new() -> Self {
+        Self {}
     }
 
     /// Build node tree from a filesystem directory
-    fn build_tree_from_fs(&mut self, mut ctx: &mut BuildContext) -> Result<Tree> {
+    fn build_tree_from_fs(
+        &mut self,
+        ctx: &mut BuildContext,
+        bootstrap_ctx: &mut BootstrapContext,
+    ) -> Result<Tree> {
         let tree_builder = FilesystemTreeBuilder::new();
 
         let node = Node::new(
@@ -87,7 +94,7 @@ impl DirectoryBuilder {
         let mut tree = Tree::new(node);
 
         tree.children = timing_tracer!(
-            { tree_builder.load_children(&mut ctx, &mut tree.node) },
+            { tree_builder.load_children(ctx, bootstrap_ctx, &mut tree.node) },
             "load_from_directory"
         )?;
 
@@ -96,30 +103,46 @@ impl DirectoryBuilder {
 }
 
 impl Builder for DirectoryBuilder {
-    fn build(&mut self, mut ctx: &mut BuildContext) -> Result<(Vec<String>, u64)> {
-        let mut blob = Blob::new(self.blob_stor.clone())?;
+    fn build(
+        &mut self,
+        ctx: &mut BuildContext,
+        bootstrap_ctx: &mut BootstrapContext,
+        blob_mgr: &mut BlobManager,
+    ) -> Result<(Vec<String>, u64)> {
+        let mut blob = Blob::new();
         let mut bootstrap = Bootstrap::new()?;
 
         // Build tree from source
-        let mut tree = self.build_tree_from_fs(&mut ctx)?;
+        let mut tree = self.build_tree_from_fs(ctx, bootstrap_ctx)?;
 
         // Build bootstrap from source
-        if ctx.f_parent_bootstrap.is_some() {
-            bootstrap.build(&mut ctx, &mut tree);
+        if bootstrap_ctx.f_parent_bootstrap.is_some() {
+            bootstrap.build(ctx, bootstrap_ctx, &mut tree);
             // Apply to parent bootstrap for layered build
-            let mut tree = bootstrap.apply(&mut ctx, None)?;
-            timing_tracer!({ bootstrap.build(&mut ctx, &mut tree) }, "build_bootstrap");
+            let mut tree = bootstrap.apply(ctx, bootstrap_ctx, blob_mgr, None)?;
+            timing_tracer!(
+                { bootstrap.build(ctx, bootstrap_ctx, &mut tree) },
+                "build_bootstrap"
+            );
         } else {
-            bootstrap.build(&mut ctx, &mut tree);
+            bootstrap.build(ctx, bootstrap_ctx, &mut tree);
         }
 
         // Dump blob file
-        let mut blob_comp_info = timing_tracer!({ blob.dump(&mut ctx) }, "dump_blob")?;
+        let mut blob_ctx = BlobContext::new(ctx.blob_id.clone(), ctx.blob_storage.clone())?;
+        let blob_index = blob_mgr.alloc_index()?;
+        timing_tracer!(
+            { blob.dump(ctx, &mut blob_ctx, blob_index, &mut bootstrap_ctx.nodes) },
+            "dump_blob"
+        )?;
+        blob.flush(&mut blob_ctx)?;
+
+        // Add new blob to blob table
+        if blob_ctx.compressed_blob_size > 0 {
+            blob_mgr.add(blob_ctx);
+        }
 
         // Dump bootstrap file
-        let (blob_ids, blob_size) = bootstrap.dump_rafsv5(&mut ctx, &mut blob_comp_info)?;
-        blob.flush(&ctx)?;
-
-        Ok((blob_ids, blob_size))
+        bootstrap.dump_rafsv5(ctx, bootstrap_ctx, blob_mgr)
     }
 }

src/bin/nydus-image/core/blob.rs
@@ -2,232 +2,53 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-use std::collections::hash_map::Entry;
-use std::collections::HashMap;
-use std::fs::{remove_file, rename, File, OpenOptions};
-use std::io::{BufWriter, Write};
 use std::os::unix::ffi::OsStrExt;
-use std::path::{Path, PathBuf};
 
-use anyhow::{Context, Error, Result};
-use sha2::{Digest, Sha256};
-use vmm_sys_util::tempfile::TempFile;
+use anyhow::{Context, Result};
+use sha2::Digest;
 
 use nydus_utils::digest::{self, DigestHasher, RafsDigest};
 
-use super::context::{BuildContext, SourceType, BUF_WRITER_CAPACITY};
+use super::context::{BlobContext, BuildContext, SourceType};
 use super::node::*;
 use crate::core::layout::BlobLayout;
 
-#[derive(Debug, Clone)]
-pub enum BlobStorage {
-    // Won't rename user's specification
-    SingleFile(PathBuf),
-    // Will rename it from tmp file as user didn't specify a name.
-    BlobsDir(PathBuf),
-}
-
-pub struct BlobBufferWriter {
-    file: BufWriter<File>,
-    blob_stor: BlobStorage,
-    // Keep this because tmp file will be removed automatically when it is dropped.
-    // But we will rename/link the tmp file before it is removed.
-    tmp_file: Option<TempFile>,
-}
-
-impl BlobBufferWriter {
-    pub fn new(blob_stor: BlobStorage) -> Result<Self> {
-        match blob_stor {
-            BlobStorage::SingleFile(ref p) => {
-                let b = BufWriter::with_capacity(
-                    BUF_WRITER_CAPACITY,
-                    OpenOptions::new()
-                        .write(true)
-                        .create(true)
-                        .truncate(true)
-                        .open(p)?,
-                );
-                Ok(Self {
-                    file: b,
-                    blob_stor,
-                    tmp_file: None,
-                })
-            }
-            BlobStorage::BlobsDir(ref p) => {
-                // Better we can use open(2) O_TMPFILE, but for compatibility sake, we delay this job.
-                // TODO: Blob dir existence?
-                let tmp = TempFile::new_in(&p)?;
-                let tmp2 = tmp.as_file().try_clone()?;
-                Ok(Self {
-                    file: BufWriter::with_capacity(BUF_WRITER_CAPACITY, tmp2),
-                    blob_stor,
-                    tmp_file: Some(tmp),
-                })
-            }
-        }
-    }
-
-    pub fn write_all(&mut self, buf: &[u8]) -> Result<()> {
-        self.file.write_all(buf).map_err(|e| anyhow!(e))
-    }
-
-    fn release(self, name: Option<&str>) -> Result<()> {
-        let mut f = self.file.into_inner()?;
-        f.flush()?;
-
-        if let Some(n) = name {
-            if let BlobStorage::BlobsDir(s) = &self.blob_stor {
-                // NOTE: File with same name will be deleted ahead of time.
-                // So each newly generated blob can be stored.
-                let might_exist_path = Path::new(s).join(n);
-                if might_exist_path.exists() {
-                    remove_file(&might_exist_path)?;
-                }
-
-                // Safe to unwrap as `BlobsDir` must have `tmp_file` created.
-                rename(self.tmp_file.unwrap().as_path(), might_exist_path)
-                    .map_err(|e| anyhow!("Rename blob to {} failed. error: {:?} ", n, e))?;
-            }
-        } else if let BlobStorage::SingleFile(s) = &self.blob_stor {
-            // `new_name` is None means no blob is really built, perhaps due to dedup.
-            // We don't want to puzzle user, so delete it from here.
-            // In the future, FIFO could be leveraged, don't remove it then.
-            remove_file(s)?;
-        }
-
-        Ok(())
-    }
-}
-
-impl Write for BlobBufferWriter {
-    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        self.file.write(buf)
-    }
-
-    fn flush(&mut self) -> std::io::Result<()> {
-        self.file.flush()
-    }
-}
-
-pub struct BlobInfoEntry {
-    chunk_count: u32,
-    chunk_ref_count: u64,
-}
-
-#[derive(Default)]
-pub struct BlobInfoMap {
-    /// Store the number of chunks in blob, it's HashMap<blob_index, chunk_count>.
-    map: HashMap<u32, BlobInfoEntry>,
-}
-
-impl BlobInfoMap {
-    /// Allocate a count index sequentially by the index of blob table.
-    pub fn alloc_index(&mut self, blob_index: u32) -> Result<u32> {
-        match self.map.entry(blob_index) {
-            Entry::Occupied(entry) => {
-                let info = entry.into_mut();
-                let index = info.chunk_count;
-                info.chunk_count = index.checked_add(1).ok_or_else(|| {
-                    Error::msg("the number of chunks in blob exceeds the u32 limit")
-                })?;
-                Ok(index)
-            }
-            Entry::Vacant(entry) => {
-                entry.insert(BlobInfoEntry {
-                    chunk_count: 1,
-                    chunk_ref_count: 0,
-                });
-                Ok(0)
-            }
-        }
-    }
-
-    /// Increase the chunk reference count of blob.
-    pub fn inc_ref_count(&mut self, blob_index: u32) {
-        match self.map.entry(blob_index) {
-            Entry::Occupied(entry) => {
-                let info = entry.into_mut();
-                info.chunk_ref_count = info.chunk_ref_count.saturating_add(1);
-            }
-            Entry::Vacant(entry) => {
-                entry.insert(BlobInfoEntry {
-                    chunk_count: 0,
-                    chunk_ref_count: 1,
-                });
-            }
-        }
-    }
-
-    /// Get the number of counts in a blob by the index of blob table.
-    pub fn count(&self, blob_index: u32) -> Option<u32> {
-        self.map.get(&blob_index).map(|v| v.chunk_count)
-    }
-}
-
-pub struct BlobCompInfo {
-    pub blob_hash: Sha256,
-    pub blob_size: u64,
-    pub blob_readahead_size: u64,
-    pub compressed_blob_size: u64,
-    pub compress_offset: u64,
-    pub decompressed_blob_size: u64,
-    pub decompress_offset: u64,
-}
-
-impl BlobCompInfo {
-    pub fn new() -> Self {
-        BlobCompInfo {
-            blob_hash: Sha256::new(),
-            blob_size: 0,
-            blob_readahead_size: 0,
-            compressed_blob_size: 0,
-            compress_offset: 0,
-            decompressed_blob_size: 0,
-            decompress_offset: 0,
-        }
-    }
-}
-
-pub struct Blob {
-    writer: BlobBufferWriter,
-    /// The size of newly generated blob. It might be ZERO if everything is the same with upper layer.
-    blob_size: u64,
-}
+pub struct Blob {}
 
 impl Blob {
-    pub fn new(bs: BlobStorage) -> Result<Self> {
-        Ok(Self {
-            writer: BlobBufferWriter::new(bs)?,
-            blob_size: 0,
-        })
+    pub fn new() -> Self {
+        Self {}
     }
 
     /// Dump blob file and generate chunks
-    pub fn dump(&mut self, ctx: &mut BuildContext) -> Result<BlobCompInfo> {
-        let mut blob_comp_info = BlobCompInfo::new();
-
-        ctx.blob_index = ctx.blob_table.entries.len() as u32;
-
+    pub fn dump(
+        &mut self,
+        ctx: &BuildContext,
+        mut blob_ctx: &mut BlobContext,
+        blob_index: u32,
+        nodes: &mut Vec<Node>,
+    ) -> Result<()> {
         match ctx.source_type {
             SourceType::Directory => {
-                let (inodes, prefetch_entries) = BlobLayout::layout_blob_simple(ctx)?;
+                let (inodes, prefetch_entries) =
+                    BlobLayout::layout_blob_simple(&ctx.prefetch, nodes)?;
                 for (idx, inode) in inodes.iter().enumerate() {
-                    let node = &ctx.nodes[*inode];
+                    let node = &mut nodes[*inode];
+                    let size = node
+                        .dump_blob(ctx, blob_ctx, blob_index)
+                        .context("failed to dump blob chunks")?;
                     if idx < prefetch_entries {
                         debug!("[{}]\treadahead {}", node.overlay, node);
                     } else {
                         debug!("[{}]\t{}", node.overlay, node);
                     }
-                    let size = Node::dump_blob(*inode, ctx, &mut self.writer, &mut blob_comp_info)
-                        .context("failed to dump blob chunks")?;
-                    blob_comp_info.blob_size += size;
                     if idx < prefetch_entries {
-                        blob_comp_info.blob_readahead_size += size;
+                        blob_ctx.blob_readahead_size += size;
                     }
                 }
             }
             SourceType::StargzIndex => {
-                for node in &mut ctx.nodes {
+                for node in nodes {
                     if node.overlay.is_lower_layer() {
                         continue;
                     } else if node.is_symlink() {
@@ -239,7 +60,7 @@ impl Blob {
                         // Set blob index and inode digest for upper nodes
                         let mut inode_hasher = RafsDigest::hasher(digest::Algorithm::Sha256);
                         for chunk in node.chunks.iter_mut() {
-                            (*chunk).blob_index = ctx.blob_index;
+                            (*chunk).blob_index = blob_index;
                             inode_hasher.digest_update(chunk.block_id.as_ref());
                         }
                         node.inode.i_digest = inode_hasher.digest_finalize();
@@ -248,17 +69,23 @@ impl Blob {
             }
         }
 
-        self.blob_size = blob_comp_info.blob_size;
+        // Name blob id by blob hash if not specified.
+        if blob_ctx.blob_id.is_empty() {
+            blob_ctx.blob_id = format!("{:x}", blob_ctx.blob_hash.clone().finalize());
+        }
 
-        Ok(blob_comp_info)
+        Ok(())
     }
 
-    pub fn flush(self, ctx: &BuildContext) -> Result<()> {
-        let blob_id = if self.blob_size > 0 {
-            Some(ctx.blob_id.as_str())
+    pub fn flush(self, blob_ctx: &mut BlobContext) -> Result<()> {
+        let blob_id = if blob_ctx.compressed_blob_size > 0 {
+            Some(blob_ctx.blob_id.as_str())
         } else {
             None
         };
-        self.writer.release(blob_id)
+        if let Some(writer) = blob_ctx.writer.take() {
+            writer.release(blob_id)?;
+        }
+        Ok(())
     }
 }

commit 1e3b2fad37c294509826bc385c42ada687fb07af

Builder: fix duplicated blobs in table
We found duplicated blob id appears in blob table, the root cause is
global (cross all layers of one image) chunk deduplication is broken,
which is caused by a previous commit.

This fixup move chunk_cache from blob context to blob manager, make
it becomes a global chunk hashmap.

Fixes: 1b6642d1 ("Builder: refactor with build context")

Signed-off-by: Yan Song <imeoer@linux.alibaba.com>

contrib/nydusify/pkg/checker/rule/bootstrap.go
@@ -71,9 +71,11 @@ func (rule *BootstrapRule) Validate() error {
 	// the blob list recorded in blob table of bootstrap
 	if !reflect.DeepEqual(bootstrap.Blobs, blobListInAnnotation) {
 		return fmt.Errorf(
-			"nydus blob list in bootstrap(%d) does not match with manifest(%d)'s",
+			"nydus blob list in bootstrap(%d) does not match with manifest(%d)'s, %v != %v",
 			len(bootstrap.Blobs),
 			len(blobListInAnnotation),
+			bootstrap.Blobs,
+			blobListInAnnotation,
 		)
 	}
 

src/bin/nydus-image/builder/directory.rs
@@ -135,7 +135,15 @@ impl Builder for DirectoryBuilder {
         }
         let blob_index = blob_mgr.alloc_index()?;
         timing_tracer!(
-            { blob.dump(ctx, &mut blob_ctx, blob_index, &mut bootstrap_ctx.nodes) },
+            {
+                blob.dump(
+                    ctx,
+                    &mut blob_ctx,
+                    blob_index,
+                    &mut bootstrap_ctx.nodes,
+                    &mut blob_mgr.chunk_cache,
+                )
+            },
             "dump_blob"
         )?;
         blob.flush(&mut blob_ctx)?;

src/bin/nydus-image/core/blob.rs
@@ -6,8 +6,10 @@ use std::os::unix::ffi::OsStrExt;
 
 use anyhow::{Context, Result};
 use sha2::Digest;
+use std::collections::HashMap;
 
 use nydus_utils::digest::{self, DigestHasher, RafsDigest};
+use rafs::metadata::layout::v5::RafsV5ChunkInfo;
 
 use super::context::{BlobContext, BuildContext, SourceType};
 use super::node::*;
@@ -27,6 +29,7 @@ impl Blob {
         mut blob_ctx: &mut BlobContext,
         blob_index: u32,
         nodes: &mut Vec<Node>,
+        chunk_cache: &mut HashMap<RafsDigest, RafsV5ChunkInfo>,
     ) -> Result<()> {
         match ctx.source_type {
             SourceType::Directory => {
@@ -35,7 +38,7 @@ impl Blob {
                 for (idx, inode) in inodes.iter().enumerate() {
                     let node = &mut nodes[*inode];
                     let size = node
-                        .dump_blob(ctx, blob_ctx, blob_index)
+                        .dump_blob(ctx, blob_ctx, blob_index, chunk_cache)
                         .context("failed to dump blob chunks")?;
                     if idx < prefetch_entries {
                         debug!("[{}]\treadahead {}", node.overlay, node);

src/bin/nydus-image/core/bootstrap.rs
@@ -2,7 +2,6 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-use std::collections::HashMap;
 use std::convert::TryFrom;
 use std::ffi::OsString;
 use std::mem::size_of;
@@ -202,11 +201,10 @@ impl Bootstrap {
         // Reuse lower layer blob table,
         // we need to append the blob entry of upper layer to the table
         blob_mgr.from_blob_table(rs.superblock.get_blob_table().as_ref());
-        let mut chunk_cache = HashMap::new();
 
         // Build node tree of lower layer from a bootstrap file, drop by to add
         // chunks of lower node to chunk_cache for chunk deduplication on next.
-        let tree = Tree::from_bootstrap(&rs, Some(&mut chunk_cache))
+        let tree = Tree::from_bootstrap(&rs, Some(&mut blob_mgr.chunk_cache))
             .context("failed to build tree from bootstrap")?;
 
         Ok(tree)

src/bin/nydus-image/core/context.rs
@@ -183,8 +183,6 @@ pub struct BlobContext {
     pub blob_layout: BlobLayout,
     /// Scratch data buffer for reading from/writing to disk files.
     pub chunk_data_buf: Vec<u8>,
-    /// Store all chunk digest for chunk deduplicate during build.
-    pub chunk_cache: HashMap<RafsDigest, RafsV5ChunkInfo>,
     /// ChunkDict which would be loaded when builder start
     pub chunk_dict: Option<Arc<dyn ChunkDict>>,
 
@@ -205,7 +203,6 @@ impl BlobContext {
             chunk_count: 0,
             blob_layout: BlobLayout::new(),
             chunk_data_buf: vec![0u8; RAFS_MAX_BLOCK_SIZE as usize],
-            chunk_cache: HashMap::new(),
             chunk_dict: None,
             writer,
         }
@@ -255,6 +252,8 @@ impl BlobContext {
 pub struct BlobManager {
     blobs: Vec<BlobContext>,
     chunk_dict: Option<Arc<dyn ChunkDict>>,
+    /// Store all chunk digest for chunk deduplicate during build.
+    pub chunk_cache: HashMap<RafsDigest, RafsV5ChunkInfo>,
 }
 
 impl BlobManager {
@@ -262,6 +261,7 @@ impl BlobManager {
         Self {
             blobs: Vec::new(),
             chunk_dict: None,
+            chunk_cache: HashMap::new(),
         }
     }
 

src/bin/nydus-image/core/node.rs
@@ -4,6 +4,7 @@
 
 //! Node structure to store information for RAFS file system inode.
 
+use std::collections::HashMap;
 use std::ffi::{OsStr, OsString};
 use std::fmt;
 use std::fs::{self, File};
@@ -235,6 +236,7 @@ impl Node {
         ctx: &BuildContext,
         blob_ctx: &mut BlobContext,
         blob_index: u32,
+        chunk_cache: &mut HashMap<RafsDigest, RafsV5ChunkInfo>,
     ) -> Result<u64> {
         if self.is_dir() {
             return Ok(0);
@@ -279,11 +281,11 @@ impl Node {
                     if let Some(c) = chunk_dict.get_chunk(&chunk.block_id) {
                         Some(c)
                     } else {
-                        blob_ctx.chunk_cache.get(&chunk.block_id)
+                        chunk_cache.get(&chunk.block_id)
                     }
                 } else {
                     // get from build chunk cache
-                    blob_ctx.chunk_cache.get(&chunk.block_id)
+                    chunk_cache.get(&chunk.block_id)
                 }
             };
 
@@ -356,7 +358,7 @@ impl Node {
             chunk.index = blob_ctx.alloc_index()?;
 
             // Cache chunk digest info
-            blob_ctx.chunk_cache.insert(chunk.block_id, chunk);
+            chunk_cache.insert(chunk.block_id, chunk);
             self.chunks.push(chunk);
 
             trace!(